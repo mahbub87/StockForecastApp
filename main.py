5666dabc29f8a7d7dce5d58ec4ff7118fb929a36
mahbub87 <98980090+mahbub87@users.noreply.github.com>
2022-08-15 02:23:02 -0400

    Update main.py

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ forecast = m.predict(future)
 st.subheader('Forecast data')
 st.write(forecast.tail())
 
-st.write(f'Forecast plot for {n_years} years')
+st.write(f'Stock Forecast')
 fig1 = plot_plotly(m, forecast)
 st.plotly_chart(fig1)
 
