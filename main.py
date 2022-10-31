bb86c27c42fe187611b74ff8003c6e4dce158bd0
mahbub87 <98980090+mahbub87@users.noreply.github.com>
2022-10-31 00:38:27 -0400

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ st.title('Stock Forecast App')
 usrInput = st.text_input("Input a Stock Symbol",max_chars=5)
 
 stocks = ('GOOG', 'AAPL', 'MSFT', 'GME', 'TSLA', st.text_input("Input a Stock Symbol",max_chars=5))
-
+selected_stock = usrInput
 selected_stock = st.selectbox('Select dataset for prediction', stocks)
 
 n_years = st.slider('Years of prediction:', 1, 4)
@@ -57,4 +57,5 @@ st.write(forecast.tail())
 st.write(f'Stock Forecast')
 fig1 = plot_plotly(m, forecast)
 st.plotly_chart(fig1)
+
 
